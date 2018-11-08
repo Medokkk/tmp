e9414823b948a0b3704a2ea988ce7de84e9e3756
Vladislav <vlad.yurash@gmail.com>
2018-11-08 18:02:37 +0300

    fixes by pp

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -1,1 +1,3 @@
 import './index.sass';
+
+console.log('attach fonts');
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,9 @@ import './index.pug';
 import './introduce';
 import './advantages';
 import './gallery';
+import './team';
+import './mailing';
+import './mailus';
 
 export default () => {
 
